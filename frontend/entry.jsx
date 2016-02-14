ba9a7777fd8d36fb802ac2991df644f91fa3ce50
spiffysparrow <tracykmullen@gmail.com>
2016-02-14 08:53:45 -0800

    |

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,16 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 // var MyComponent = require('./components/myComponent.jsx');
-// var d3 = require('./libs/d3.js');
+var d3 = require('./libs/d3.js');
 
 document.addEventListener("DOMContentLoaded", function () {
   // ReactDOM.render(<MyComponent />, document.getElementById('main'));
 });
 
 console.log("boo");
-// debugger
-// FullClient client = new FullClient("b15cf6d0-d2ed-11e5-8378-4dad29be0fab", "en_associative");
-// orExpression = {"or": [{"term": "brain"}, {"text": "a region of the cerebral cortex"}]}
-// terms = client.getSimilarTermsForExpression(json.dumps(orExpression))
-// console.log(terms);
-// console.log("hgjhg");
