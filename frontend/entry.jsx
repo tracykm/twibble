6a776762cd42ecdb5ac54a37784f2fe67478e00f
spiffysparrow <tracykmullen@gmail.com>
2016-02-15 20:51:24 -0800

    |

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,21 +1,21 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-// var MyComponent = require('./components/myComponent.jsx');
-// var Comparison = require('./components/comparison.jsx');
-// var d3 = require('./libs/d3.js');
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<CommentBox />, document.getElementById('main'));
-});
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// // var MyComponent = require('./components/myComponent.jsx');
+// // var Comparison = require('./components/comparison.jsx');
+// // var d3 = require('./libs/d3.js');
+//
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         Hello, world! I am a CommentBox.
+//       </div>
+//     );
+//   }
+// });
+//
+// document.addEventListener("DOMContentLoaded", function () {
+//   ReactDOM.render(<CommentBox />, document.getElementById('main'));
+// });
 
 console.log("boo");
