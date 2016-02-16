ad3d284d974fe9cbb3de7dacb72e960d0bcc73e1
spiffysparrow <tracykmullen@gmail.com>
2016-02-15 20:43:11 -0800

    sad comment box test

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var MyComponent = require('./components/myComponent.jsx');
-var Comparison = require('./components/comparison.jsx');
-var d3 = require('./libs/d3.js');
+// var MyComponent = require('./components/myComponent.jsx');
+// var Comparison = require('./components/comparison.jsx');
+// var d3 = require('./libs/d3.js');
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+  ReactDOM.render(<CommentBox />, document.getElementById('main'));
 });
 
 console.log("boo");
