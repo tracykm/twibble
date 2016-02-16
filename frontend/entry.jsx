b61b72289123a31417c9443b56a7b3e7b9c2d1de
spiffysparrow <tracykmullen@gmail.com>
2016-02-15 20:22:36 -0800

    |

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,9 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var MyComponent = require('./components/myComponent.jsx');
-// // var Comparison = require('./components/comparison.jsx');
-// var d3 = require('./libs/d3.js');
-//
+var React = require('react');
+var ReactDOM = require('react-dom');
+var MyComponent = require('./components/myComponent.jsx');
+// var Comparison = require('./components/comparison.jsx');
+var d3 = require('./libs/d3.js');
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   ReactDOM.render(<MyComponent />, document.getElementById('main'));
 // });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-
 var path = require("path");
+
+// var context = require.context('./src', true, /-test\.jsx?$/);
+// context.keys().forEach(context);
 
 module.exports = {
   context: __dirname,
@@ -10,5 +12,25 @@ module.exports = {
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
   devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.jsx$/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      }
+    ]
+  }
 };
