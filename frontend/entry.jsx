f13294f865ef295fd8200b1c9bae09f5aba4e62a
spiffysparrow <tracykmullen@gmail.com>
2016-02-15 19:47:14 -0800

    |

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MyComponent = require('./components/myComponent.jsx');
-// var Comparison = require('./components/comparison.jsx');
-var d3 = require('./libs/d3.js');
-
-document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<MyComponent />, document.getElementById('main'));
-});
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var MyComponent = require('./components/myComponent.jsx');
+// // var Comparison = require('./components/comparison.jsx');
+// var d3 = require('./libs/d3.js');
+//
+// document.addEventListener("DOMContentLoaded", function () {
+//   ReactDOM.render(<MyComponent />, document.getElementById('main'));
+// });
 
 console.log("boo");
